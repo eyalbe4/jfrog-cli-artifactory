29ef4f96e2748cf58a3f15f139182b0daf4db4a6
eyalbe4 <eyalbenmoshe@jfrog.com>
2024-02-17 16:26:12 +0200

    Modify commands hierarchy

--- cli/cli.go
+++ cli/cli.go
@@ -1,19 +1,13 @@
 package cli
 
 import (
-	"github.com/jfrog/jfrog-cli-core/v2/common/cliutils"
 	"github.com/jfrog/jfrog-cli-core/v2/plugins/components"
 )
 
 func GetJfrogCliArtifactoryApp() components.App {
 	app := components.CreateEmbeddedApp(
 		"artifactory",
-		nil,
+		getCommands(),
 	)
-	app.Subcommands = append(app.Subcommands, components.Namespace{
-		Name:        string(cliutils.Rt),
-		Description: "Artifactory commands.",
-		Commands:    getCommands(),
-	})
 	return app
 }
